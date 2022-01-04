ff906564cf15d5781c8feb5030ddd617d7c7c72f
Marc Ruben Epacta <marcrubenepacta@gmail.com>
2022-01-04 13:56:44 +0800

    Date Difference Calculator

---
+++ Calculator/Calculator/DateDif.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Calculator
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DateDif : ContentPage
+    {
+        public DateDif()
+        {
+            InitializeComponent();
+        }
+
+        void OnDateSelected(object sender, DateChangedEventArgs args)
+        {
+            Recalculate();
+        }
+
+        void OnSwitchToggled(object sender, ToggledEventArgs args)
+        {
+            Recalculate();
+        }
+
+        void Recalculate()
+        {
+            TimeSpan timeSpan = endDatePicker.Date - startDatePicker.Date +
+                (includeSwitch.IsToggled ? TimeSpan.FromDays(1) : TimeSpan.Zero);
+
+            resultLabel.Text = String.Format("{0} day{1} between dates",
+                                             timeSpan.Days, timeSpan.Days == 1 ? "" : "s");
+        }
+    }
+}
